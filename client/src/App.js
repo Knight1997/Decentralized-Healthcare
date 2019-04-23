6f23a0ad9f49b6a2ef4d19550ee1eb28091569b6
knight1997 <utkarsh.sinha97@gmail.com>
2019-04-23 21:48:04 +0530

    Image Tag Removed

--- client/src/App.js
+++ client/src/App.js
@@ -338,9 +338,9 @@ class App extends Component {
         <main className="container">
           <div className="pure-g">
             <div className="pure-u-1-1">
-              <h1>Your Image</h1>
+              
               <p>This Precriptions are stored on IPFS & its IPFS HASH is stored on The Ethereum Blockchain!</p>
-              <img src={`https://ipfs.io/ipfs/${this.state.ipfsHash}`} alt=""/>
+              
               <h2>Upload Precription File</h2>
               <form onSubmit={this.onSubmit} >
                 <label>Enter Patient ID </label>
